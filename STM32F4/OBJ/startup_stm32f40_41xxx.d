df5ca185ac2d3b6a86035653aa2d806ed391543f
ziqian-L <ziqian303030@163.com>
2023-02-16 21:00:21 +0800

    新增STM32F407开发模板

---
+++ STM32F4/OBJ/startup_stm32f40_41xxx.d
@@ -0,0 +1,1 @@
+..\obj\startup_stm32f40_41xxx.o: ..\CODE\startup_stm32f40_41xxx.s
---
+++ STM32F4/Project/main.c
@@ -0,0 +1,14 @@
+#include "stm32f4xx.h"
+#include "delay.h"
+#include "sys.h"
+#include "usart.h"
+
+
+#define VCC 3.3
+
+int main(void)
+{
+    while(1)
+    {
+    }
+}
